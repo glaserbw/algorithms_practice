c27d681bc689eb0700b89c9ad6c185a51fab6ae6
Brian Glaser <brianglaser@Brians-MacBook-Pro.local>
2018-11-05 19:16:05 -0800

    updating sum pivot problem

--- sum_pivot.py
+++ sum_pivot.py
@@ -1,14 +1,28 @@
 arr = [1, 100, 50, -51, 1, 1]
 
 def find_pivot(arr):
+    arrLeft = 0
+    arrRight = 0
     for i in range(len(arr)):
-        arrLeft = sum(arr[0:i])
-        # print(arrLeft)
-        arrRight = sum(arr[i:len(arr)])
+        arrLeft = 0
+        arrRight = 0
+        for j in range(i):
+            arrLeft += arr[j]
+        for j in range(i+1, len(arr)):
+            arrRight += arr[j]
         if arrRight == arrLeft:
             return i
-            
+    return -1  
     # return -1 
-
-
+ 
 print(find_pivot(arr))
+
+# def find_pivotnew(arr):
+#     newArr = sum(arr)
+#     print(newArr)
+#     for i in range(len(arr)):
+#         test = newArr - arr[i]
+#         if test = 
+
+
+# # find_pivotnew(arr)
